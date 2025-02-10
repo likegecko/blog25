35617a993a5e81e00e6e4c1c2a3fbde4a3715ad0
kimts2627 <like.gecko1@gmail.com>
2025-02-10 23:52:11 +0900

    feat: 프로젝트 레이아웃 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,8 @@
-import Header from "@/app/components/Header";
-
 export default function Home() {
   return (
     <div>
-      <Header />
-      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <h2 className="text-4xl font-bold mb-6">홈페이지</h2>
-        <p className="text-gray-600">
-          환영합니다! 이곳은 블로그 홈페이지입니다.
-        </p>
-      </main>
+      <h2 className="text-4xl font-bold mb-6">홈페이지</h2>
+      <p className="text-gray-600">환영합니다! 이곳은 블로그 홈페이지입니다.</p>
     </div>
   );
 }

657576b209c3afd6d7c9f1af9bc77f4a90d91f08
kimts2627 <like.gecko1@gmail.com>
2025-02-11 00:40:25 +0900

    feat: 푸터 추가

---
+++ app/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="py-6 flex items-center justify-center">
+      <p className="text-xs text-gray-500">
+        © 2021–2025 Kim Taesu · Powered by Coffee and Zero Coke.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "./providers/AuthProvider";
 import Header from "./components/Header";
-
+import Footer from "./components/Footer";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -34,6 +34,7 @@ export default function RootLayout({
             <div className="w-full px-4">
               <Header />
               <main className="mt-12">{children}</main>
+              <Footer />
             </div>
           </div>
         </AuthProvider>

9270e5eaff93ceee5cc4687bd50fcdf610c185ac
kimts2627 <like.gecko1@gmail.com>
2025-02-20 23:34:54 +0900

    feat: 개인 프로필 사진 추가
    - 불필요한 정적 파일 제거

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,21 @@
+import Image from "next/image";
+
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div className="flex flex-col items-center gap-6">
+      <section className="flex flex-col items-center">
+        <div className="flex justify-center items-center rounded-full overflow-hidden w-[240px] h-[240px]">
+          <Image
+            width={400}
+            height={400}
+            src="/images/profile.png"
+            alt="profile image"
+            className="scale-120 pt-7"
+          />
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default AboutPage;

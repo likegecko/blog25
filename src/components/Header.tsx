9ceb78b61518d4abab1bb9c2c68ad8b57467e185
kimts2627 <like.gecko1@gmail.com>
2025-03-15 22:42:47 +0900

    feat: 임시 색상 제거

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,8 @@ import { SITE_TITLE } from '../consts';
 
 const Header = () => {
   return (
-    <header className="drop-shadow-lg w-full bg-white h-[40px] flex justify-center">
-      <div className="w-full max-w-4xl bg-red-100 h-full flex items-center justify-between px-4">
+    <header className="drop-shadow-sm w-full bg-white h-[60px] flex justify-center">
+      <div className="w-full max-w-4xl h-full flex items-center justify-between px-4">
         <h1 className="text-xl font-bold">{SITE_TITLE}</h1>
         <nav className="flex items-center gap-4">
           <a href="/">Home</a>

06b4c065bbce28c124534fd1bd259fb53d92bf32
kimts2627 <like.gecko1@gmail.com>
2025-03-16 02:00:55 +0900

    feat: 헤더 및 푸터 레이아웃 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,9 @@ const Header = () => {
   return (
     <header className="drop-shadow-sm w-full bg-white h-[60px] flex justify-center">
       <div className="w-full max-w-4xl h-full flex items-center justify-between px-4">
-        <h1 className="text-xl font-bold">{SITE_TITLE}</h1>
+        <a href="/">
+          <h1 className="text-xl font-bold">{SITE_TITLE}</h1>
+        </a>
         <nav className="flex items-center gap-4">
           <a href="/">Home</a>
           <a href="/about">About</a>
